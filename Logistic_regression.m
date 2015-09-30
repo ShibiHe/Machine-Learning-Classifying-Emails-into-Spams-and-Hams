edf9b6998ef0861b6e80a8552ffb6d178c145c88
Frank He <frankheshibi@gmail.com>
2015-09-30 14:43:43 +0800

    2nd version

--- Logistic_regression.m
+++ Logistic_regression.m
@@ -2,15 +2,33 @@
 clear;
 load train_matrix.txt;
 load test_matrix.txt;
-%test filter: some columns are all zero.
+
+%% train filter: some columns are all zero.
+[row, col]=find(train_matrix>0);
+original_train_emails=size(train_matrix,2);
+I(size(train_matrix,2))=false;
+for i=1:length(col)
+    I(col(i))=true;
+end
+I=logical(I);
+train_matrix=train_matrix(:,I);
+clear I;
+
+%% test filter: some columns are all zero.
 [row, col]=find(test_matrix>0);
-original_emails=size(test_matrix,2);
+original_test_emails=size(test_matrix,2);
 I(size(test_matrix,2))=false;
 for i=1:length(col)
     I(col(i))=true;
 end
 I=logical(I);
 test_matrix=test_matrix(:,I);
+
+%% add intercept term into train and test(not good)
+% train_matrix(end+1,:)=train_matrix(end,:);
+% train_matrix(end-1,:)=1;
+% test_matrix(end+1,:)=test_matrix(end,:);
+% test_matrix(end-1,:)=1;
 
 %% initialization
 I=randperm(size(train_matrix,2));
@@ -22,9 +40,9 @@ n=size(train_matrix,2);
 theta = rand(m,1)*0.1;  %this shows the initialization's influence
 
 %% gradient check
-disp(grad_check(@logistic_regression_vec,theta,10,X,y));
+disp(grad_check(@logistic_regression_vec,theta,5,X,y));
 %% start training
-iterations=1200;
+iterations=1000;
 eta=0.3;
 fprintf('features=%d;\tsamples=%d;\tlearning rate is %f\n',m,n,eta);
 for i=1:iterations
@@ -44,7 +62,9 @@ for i=1:length(answer)
 end
 answer=answer-y;
 wrong=length(find(answer~=0));
-fprintf('training accuracy is %d/%d   %4.2f\n',wrong,n,((n-wrong)/n)*100);
+total=size(train_matrix,2);
+fprintf('training lost emails=%d\n',original_train_emails-total);
+fprintf('training accuracy is %d/%d   %4.2f\n',wrong,total,((total-wrong)/total)*100);
 
 %% start testing
 X=sparse(test_matrix(1:end-1,:));
@@ -61,4 +81,4 @@ answer=answer-y;
 wrong=length(find(answer~=0));
 total=size(test_matrix,2);
 fprintf('lost emails=%d\ntesting accuracy is %d/%d   %4.2f\n', ...
-original_emails-total,wrong,total,((total-wrong)/total)*100);
+original_test_emails-total,wrong,total,((total-wrong)/total)*100);
